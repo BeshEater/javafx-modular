e77a9901ce6237bce2247c738349ebc7425f9253
BeshEater <beinzone@gmail.com>
2020-01-20 03:39:35 +0600

    Add module-info generation for runtime image

--- src/main/java/com/besheater/javafxmodular/controller/SecondaryController.java
+++ src/main/java/com/besheater/javafxmodular/controller/SecondaryController.java
@@ -4,11 +4,22 @@ import com.besheater.javafxmodular.WindowManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import org.apache.commons.lang3.SystemUtils;
 
 public class SecondaryController {
 
     @FXML
+    private Label textLabel;
+
+    @FXML
     private Button closeBtn;
+
+    @FXML
+    public void initialize() {
+        String text = SystemUtils.OS_NAME + " | " + SystemUtils.OS_VERSION;
+        textLabel.setText(text);
+    }
 
     @FXML
     private void closeBtnPressed(ActionEvent event) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.besheater.filedownloader {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.commons.lang3;
 
     opens com.besheater.javafxmodular to javafx.fxml;
     opens com.besheater.javafxmodular.controller to javafx.fxml;
